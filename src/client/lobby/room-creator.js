43bde1c592e71415ced23a06aad94a03d9cb444d
Jamie Zafar <jo13358@my.bristol.ac.uk>
2016-07-05 15:22:12 +0100

    added input area for username and for new room name and also buttons for both

--- src/client/lobby/room-creator.js
+++ src/client/lobby/room-creator.js
@@ -7,6 +7,15 @@ export default class RoomCreator extends Component {
         return (
             <div className="col-lg-4 text-center">
                 <h2>Create Room & Set Username</h2>
+                <div className="row">
+                    <input type="text" placeholder="Please type your new username"></input>
+                    <button className="btn btn-primary">Set Username</button>
+                </div>
+                <br></br>
+                <div className="row">
+                    <input type="text" placeholder="Please type new room name"></input>
+                    <button className="btn btn-primary">Create Room</button>
+                </div>
             </div>
         );
     }
