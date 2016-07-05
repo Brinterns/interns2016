030be56c7dd8cdf445ae0aeb227e2a124496b543
Jamie Zafar <jo13358@my.bristol.ac.uk>
2016-07-05 12:21:06 +0100

    improved styling of user list element

--- src/client/lobby/user-list.js
+++ src/client/lobby/user-list.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 import config from '../config/config';
 
+import style from './lobby.scss';
+
 export default function UserList(props) {
     return (
         <div className="col-lg-4 text-center">
             <h2>User List</h2>
-            <div>
+            <div className={style['list-areas']}>
                 {props.lobbyUsers.map( result => {
                     return (
                         <p className='row' key={result['id']}>
