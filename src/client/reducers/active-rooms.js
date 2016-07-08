9642e4c4b9767fe2d7fcfd13a7fefbecaaa2105e
Jamie Zafar <jo13358@my.bristol.ac.uk>
2016-07-08 09:40:33 +0100

    Removed Array.from and slice() from reducers altogether

--- src/client/reducers/active-rooms.js
+++ src/client/reducers/active-rooms.js
@@ -3,7 +3,7 @@ import * as types from '../actions/types';
 export default (state = [], action) => {
     switch(action.type) {
         case types.UPDATE_ROOM_LIST:{
-            return Array.from(action.payload);
+            return action.payload;
         }
         default:{
             return state;
--- src/client/reducers/lobby-users.js
+++ src/client/reducers/lobby-users.js
@@ -3,7 +3,7 @@ import * as types from '../actions/types';
 export default (state = [], action) => {
     switch (action.type) {
         case types.UPDATE_LOBBY_LIST: {
-            return action.payload.slice();
+            return action.payload;
         }
         default: {
             return state;
