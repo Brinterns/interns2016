07d0e1a98536fb93bd0c23b24b51d9a3ba788d8b
Jamie Zafar <jo13358@my.bristol.ac.uk>
2016-07-08 09:33:32 +0100

    Changed slice to Array.from in active-rooms.js

--- src/client/reducers/active-rooms.js
+++ src/client/reducers/active-rooms.js
@@ -3,7 +3,7 @@ import * as types from '../actions/types';
 export default (state = [], action) => {
     switch(action.type) {
         case types.UPDATE_ROOM_LIST:{
-            return action.payload.slice();
+            return Array.from(action.payload);
         }
         default:{
             return state;
