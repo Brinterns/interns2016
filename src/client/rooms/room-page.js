712eb1cf8ba45299d9ae0d38149210f911755ceb
Jamie Zafar <jo13358@my.bristol.ac.uk>
2016-07-11 14:25:14 +0100

    Added a colon after the ROOM on room-page

--- src/client/rooms/room-page.js
+++ src/client/rooms/room-page.js
@@ -4,7 +4,7 @@ export default class RoomPage extends Component {
     render() {
         return (
             <div className="col-lg-4 text-center">
-            <h2>{`Room ${this.props.params.id}`}</h2>
+            <h2>{`Room: ${this.props.params.id}`}</h2>
             </div>
         );
     }
