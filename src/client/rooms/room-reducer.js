e7b7468cc9736cd132a150aa9e4d1d6482fcb603
Jamie Zafar <jo13358@my.bristol.ac.uk>
2016-07-15 17:25:09 +0100

    Bound the bindings twicely

--- src/client/rooms/room-reducer.js
+++ src/client/rooms/room-reducer.js
@@ -2,16 +2,18 @@ import { UPDATE_ROOM_USERS, GET_ROOM_DATA } from './room-actions';
 
 const initialState = {
 	users: [],
-	data: {}
+	data: {},
+	username: '',
+	roomname: ''
 };
 
 const room = (state = initialState, action) => {
     switch (action.type) {
-        case UPDATE_ROOM_USERS: 
+        case UPDATE_ROOM_USERS:
         	return Object.assign({}, state, {
         		users: action.payload
         	});
-        case GET_ROOM_DATA: 
+        case GET_ROOM_DATA:
             return Object.assign({}, state, {
             	data: action.payload
             });
