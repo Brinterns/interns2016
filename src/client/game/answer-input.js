73888ded6894f02668d9d7cf19c85643c5d3a4d9
Jamie Zafar <jo13358@my.bristol.ac.uk>
2016-07-18 18:40:22 +0100

    Generates text boxes, up and down arrows do the thing, the thing thing

--- src/client/game/answer-input.js
+++ src/client/game/answer-input.js
@@ -5,15 +5,34 @@ import style from '../index.scss';
 export default class AnswerInput extends Component {
     componentWillMount() {
         this.setState({
-            answerList: ['']
+            answerList: [''],
+            focusIndex: 0
         });
     }
 
     handleEnterPress(event) {
-        if(event.keyCode === 13 || event.which === 13) {
-            this.setState({
-                answerList: [...this.state.answerList, 'Enter your answer here']
-            });
+        switch (event.which) {
+            case 13: { //Enter key press
+                this.setState({
+                    answerList: [...this.state.answerList, 'Enter your answer here'],
+                    focusIndex: this.state.answerList.length
+                });
+                break;
+            }
+            case 38: {
+                let focusIndex = this.state.focusIndex === 0 ? 0 : this.state.focusIndex-1;
+                this.setState({
+                    focusIndex: focusIndex
+                });
+                break;
+            }
+            case 40: {
+                let focusIndex = this.state.focusIndex === this.state.answerList.length-1 ? this.state.answerList.length-1 : this.state.focusIndex+1;
+                this.setState({
+                    focusIndex: focusIndex
+                });
+                break;
+            }
         }
     }
 
@@ -25,19 +44,19 @@ export default class AnswerInput extends Component {
                     if(index === id) {
                         return event.target.value;
                     }
-
                     return originalAnswer;
                 })
         });
     }
 
-    textBoxGenerator(props) {
+    textBoxGenerator(props, focusIndex) {
         return (
             props.map((answer,index) => {
-                let focus = index === props.length-1;
+                let focus = index === focusIndex;
+                console.log(focus+' '+index);
                 return (
                     <div className="row">
-                        <input maxLength="18" size="30" placeholder="Enter your answer here" autoFocus={focus}
+                        <input maxLength="18" size="30" placeholder="Enter your answer here" ref={index}
                             defaultValue={answer} onChange={(event) => this.handleChange(event, index)} onKeyDown={event => this.handleEnterPress(event)}/>
                     </div>
                 );
@@ -45,12 +64,17 @@ export default class AnswerInput extends Component {
         );
     }
 
+    componentDidUpdate(){
+        let focusIndex = this.state.focusIndex;
+        this.refs[focusIndex].focus();
+    }
+
     render() {
         return (
             <div className="col-lg-12 text-center">
                 <h3 className={style.header3}>ANSWER INPUT</h3>
                 <div>
-                    {this.textBoxGenerator(this.state.answerList)}
+                    {this.textBoxGenerator(this.state.answerList, this.state.focusIndex)}
                 </div>
             </div>
         );
