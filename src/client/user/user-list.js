1f65ea3dff0c1d627a88381e5527ab5f3b4c636a
Raul Mihoc <rmihoc@scottlogic.co.uk>
2016-07-19 16:15:25 +0100

    made user data contain score. Gets initialised to 0 when entered room. Userlist now sorts by score and then alphabetically

--- src/client/user/user-list.js
+++ src/client/user/user-list.js
@@ -5,15 +5,17 @@ import config from '../config/config';
 import style from '../common/common.scss'
 
 export default function UserList(props) {
+    let users = sort(props.users);
     return (
         <div className="col-lg-4 text-center">
             <h2>User List</h2>
             <div className="col-lg-12 pre-scrollable list-group">
                 <ul>
-                    {props.users.map( result => {
+                    {users.map( result => {
                         return (
                             <li className={`list-group-item list-group-item-success ${style.space}`} key={result.id}>
                                 {result.name}
+                                {showScore(result.data.score)}
                             </li>
                         );
                     })}
@@ -21,4 +23,25 @@ export default function UserList(props) {
             </div>
         </div>
     );
+}
+
+function showScore(score){
+    if(score !== undefined){
+        return (
+            <span className='badge'>{score}</span>
+        );
+    }
+    return null
+}
+
+function sort(users) {
+    users.sort((a, b) => {
+        let score = 0-(a.data.score-b.data.score);
+        if(a.score !== undefined && b.data.score !== undefined && score !== 0)
+            return score;
+        else
+            return a.name.localeCompare(b.name);
+
+    });
+    return users;
 }
