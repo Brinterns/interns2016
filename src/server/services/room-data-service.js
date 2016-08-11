8973c65e2b4072fe3291259a36235da74cdda974
Jamie Zafar <joconnor@scottlogic.co.uk>
2016-08-11 16:04:58 +0100

    Fixed the indentation issue in room-data-service. Object.assign now used in a better way. 10/10

--- src/server/services/room-data-service.js
+++ src/server/services/room-data-service.js
@@ -4,42 +4,41 @@ module.exports = {
 }
 
 function initialRoomData(user) {
-	return {
-		creator: {
-			id: user.id,
-			name: user.name
-		},
-	    userIdList: [],
-	    started: false,
-	    answering: false,
-	    submitting: false,
-	    scores: [],
-	    letterList: {
-	        letters: [],
-	        consonantNum: 0,
-	        vowelNum: 0,
-	        disableConsonant: false,
-	        disableVowel: false
-	    },
-	    possibleAnswers: {},
-	    finalAnswerList: {}
-	}
+    return {
+        creator: {
+            id: user.id,
+            name: user.name
+        },
+        userIdList: [],
+        started: false,
+        answering: false,
+        submitting: false,
+        scores: [],
+        letterList: {
+            letters: [],
+            consonantNum: 0,
+            vowelNum: 0,
+            disableConsonant: false,
+            disableVowel: false
+        },
+        possibleAnswers: {},
+        finalAnswerList: {}
+    }
 }
 
 function newRoundData(oldData) {
-	Object.assign(oldData,{
-		answering: false,
-    	submitting: false,
-		roundEnded: false,
-    	letterList: {
-        	letters: [],
-        	consonantNum: 0,
-        	vowelNum: 0,
-        	disableConsonant: false,
-        	disableVowel: false
-    	},
-    	possibleAnswers: {},
-    	finalAnswerList: {}
-	});
-	return oldData;
+    return Object.assign({}, oldData, {
+        answering: false,
+        submitting: false,
+        roundEnded: false,
+        letterList: {
+            letters: [],
+            consonantNum: 0,
+            vowelNum: 0,
+            disableConsonant: false,
+            disableVowel: false
+        },
+        possibleAnswers: {},
+        finalAnswerList: {}
+    });
 }
