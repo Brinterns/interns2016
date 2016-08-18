6b654c3719d58ff16aa63b4e14a80de80dae61ae
Raul Mihoc <rmihoc@scottlogic.co.uk>
2016-08-18 10:00:19 +0100

    added shuffle function for the backend

---
+++ src/server/numbers-round/shuffle.js
@@ -0,0 +1,18 @@
+module.exports = shuffle;
+
+function shuffle(array) {
+    var current = array.length;
+    var index;
+    var aux;
+
+    while(current) {
+        index = Math.floor(Math.random() * current);
+        current--;
+
+        aux = array[current];
+        array[current] = array[index];
+        array[index] = aux;
+    }
+
+    return array;
+}
