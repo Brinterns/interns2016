1c8071b646ad65e978bf80a24ad318e7b541f393
JamieZafar <jo13358@my.bristol.ac.uk>
2016-08-26 11:13:53 +0100

    Added image and speech bubble to diktionary korner

--- src/client/rooms/dictionary-corner.js
+++ src/client/rooms/dictionary-corner.js
@@ -25,23 +25,28 @@ export class DictionaryCorner extends Component {
         return (
             <div className={`col-lg-12 ${style['dictionary-corner']}`} ref="dict-corner">
                 {this.state.bestAnswer.word !== undefined ? 
-                    <h2>Dictionary Corner</h2>
+                    <div className={style['susie-section']}>
+                        <h2>Dictionary Corner</h2>
+                        <img className={style.susie} src="http://wemeantwell.com/blog/wp-content/uploads/2013/03/kim-jong-un.jpg"/>
+                    </div>
                 :
                     null
                 }
-                <div className={style['dictionary-corner-word']}>
-                    {this.state.bestAnswer.word ?
-                        this.state.bestAnswer.word
-                    :
-                        null
-                    }
-                </div>
-                <div className={style['dictionary-corner-definition']}>
-                    {this.state.bestAnswer.definition ?
-                        this.state.bestAnswer.definition
-                    :
-                        null
-                    }
+                <div className={style.bubble}>
+                    <div className={style['dictionary-corner-word']}>
+                        {this.state.bestAnswer.word ?
+                            this.state.bestAnswer.word
+                        :
+                            null
+                        }
+                    </div>
+                    <div className={style['dictionary-corner-definition']}>
+                        {this.state.bestAnswer.definition ?
+                            this.state.bestAnswer.definition
+                        :
+                            null
+                        }
+                    </div>
                 </div>
             </div>
         );
