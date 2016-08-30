cb521630e552b6b0967d628037ff3329e1f29fea
Raul Mihoc <rmihoc@scottlogic.co.uk>
2016-08-30 10:46:47 +0100

    added number input tests

--- src/client/game/numbers-round/numbers-input.spec.js
+++ src/client/game/numbers-round/numbers-input.spec.js
@@ -22,45 +22,98 @@ describe('<NumbersInput />', () => {
         expect(wrapper.find('h3').text()).toEqual('Answer');
     });
 
-    it('Initialises state correctly', () => {
+    it('sets sets state correctly when an answer gets typed in the answer text area', () => {
         let wrapper = shallow(
             <NumbersInput {...props} />
         );
 
-        expect(wrapper.state().answer).toEqual('');
+        wrapper.find('textarea').simulate('change', {
+            target: {
+                value: 'answer 1'
+            }
+        });
+
+        expect(wrapper.state().answer).toEqual('answer 1');
     });
 
-    describe('Behaviour on receiving new props', () => {
-        it('Calls messageSendEquation when the sendEquation prop is first sent', () => {
-            cloakService = jasmine.createSpyObj('cloakService', ['messageSendEquation']);
-            rewire('cloakService', cloakService);
-            props = {
-                sendEquation: false
-            };
+    describe('tab key press', () => {
+        let handleTab;
+        let preventDefault;
 
+        beforeEach(() => {
+            handleTab = jasmine.createSpy('handleTab');
+            preventDefault = jasmine.createSpy('preventDefault');
+            rewire('handleTab', handleTab);
+        });
+
+        afterEach(() => {
+            resetDependency('handleTab');
+        });
+
+        it('calls "preventDefault" if a tab is pressed in the answer text area', () => {
             let wrapper = shallow(
                 <NumbersInput {...props} />
             );
+            let event = {
+                which: 9,
+                target: {
+                    value: 'answer 1'
+                },
+                preventDefault: preventDefault
+            }
 
-            wrapper.setProps({ sendEquation: true });
+            wrapper.find('textarea').simulate('keydown', event);
 
-            expect(cloakService.messageSendEquation).toHaveBeenCalled();
-            
-            resetDependency('cloakService');
+            expect(preventDefault).toHaveBeenCalled();
         });
 
-        it('Resets state.answer when the resetRound prop is first sent', () => {
-            props = {
-                resetRound: false
-            };
-
+        it('calls the "handleTab" function if a tab is pressed in the answer text area', () => {
             let wrapper = shallow(
                 <NumbersInput {...props} />
             );
+            let event = {
+                which: 9,
+                target: {
+                    value: 'answer 1'
+                },
+                preventDefault: preventDefault
+            }
 
-            wrapper.setProps({ resetRound: true });
+            wrapper.find('textarea').simulate('keydown', event);
 
-            expect(wrapper.state('answer')).toEqual('');
+            expect(handleTab).toHaveBeenCalledWith(event);
         });
+    });
+
+    it('calls cloakService.messageSendEquation when the sendEquation prop changes to true', () => {
+        let cloakService = jasmine.createSpyObj('cloakService', ['messageSendEquation']);
+        rewire('cloakService', cloakService);
+
+        let wrapper = shallow(
+            <NumbersInput {...props} />
+        );
+        wrapper.setState({
+            answer: 'answer 1'
+        });
+        wrapper.setProps({
+            sendEquation: true
+        });
+
+        expect(cloakService.messageSendEquation).toHaveBeenCalledWith('answer 1');
+        resetDependency('cloakService');
+    });
+
+    it('resets the answer to empty when the resetRound prop changes to true', () => {
+        let wrapper = shallow(
+            <NumbersInput {...props} />
+        );
+        wrapper.setState({
+            answer: 'answer 1'
+        });
+        wrapper.setProps({
+            resetRound: true
+        });
+
+        expect(wrapper.state().answer).toEqual('');
     });
 });
