9c229769f1d4892513fa9d5e1d662c972a1942f3
JamieZafar <jo13358@my.bristol.ac.uk>
2016-08-30 09:12:06 +0100

    Added some tests

--- src/client/game/numbers-round/numbers-input.js
+++ src/client/game/numbers-round/numbers-input.js
@@ -50,11 +50,9 @@ export class NumbersInput extends Component {
         return (
             <div className="col-lg-12 text-center">
                 <h3>Answer</h3>
-                <div>
-                    <textarea rows="20" cols="100" size="1000" placeholder="Answer here"
-                    onKeyDown={event => this.handleKeyPress(event)}
-                    onChange={event => this.handleChange(event)}/>
-                </div>
+                <textarea rows="20" cols="100" size="1000" placeholder="Answer here"
+                onKeyDown={event => this.handleKeyPress(event)}
+                onChange={event => this.handleChange(event)} />
             </div>
         );
     }
--- src/client/game/numbers-round/numbers-input.spec.js
+++ src/client/game/numbers-round/numbers-input.spec.js
@@ -8,6 +8,8 @@ const { rewire, resetDependency } = normaliseRewire(__RewireAPI__);
 
 describe('<NumbersInput />', () => {
     let props;
+    let cloakService;
+
     beforeEach(() => {
         props = {};
     })
@@ -18,5 +20,47 @@ describe('<NumbersInput />', () => {
         );
 
         expect(wrapper.find('h3').text()).toEqual('Answer');
+    });
+
+    it('Initialises state correctly', () => {
+        let wrapper = shallow(
+            <NumbersInput {...props} />
+        );
+
+        expect(wrapper.state().answer).toEqual('');
+    });
+
+    describe('Behaviour on receiving new props', () => {
+        it('Calls messageSendEquation when the sendEquation prop is first sent', () => {
+            cloakService = jasmine.createSpyObj('cloakService', ['messageSendEquation']);
+            rewire('cloakService', cloakService);
+            props = {
+                sendEquation: false
+            };
+
+            let wrapper = shallow(
+                <NumbersInput {...props} />
+            );
+
+            wrapper.setProps({ sendEquation: true });
+
+            expect(cloakService.messageSendEquation).toHaveBeenCalled();
+            
+            resetDependency('cloakService');
+        });
+
+        it('Resets state.answer when the resetRound prop is first sent', () => {
+            props = {
+                resetRound: false
+            };
+
+            let wrapper = shallow(
+                <NumbersInput {...props} />
+            );
+
+            wrapper.setProps({ resetRound: true });
+
+            expect(wrapper.state('answer')).toEqual('');
+        });
     });
 });
