25e626e573697bfbdb912fc24e55ede50bf260b9
JamieZafar <jo13358@my.bristol.ac.uk>
2016-09-02 14:07:22 +0100

    Rebased

--- src/client/game/conundrum-round/conundrum-input.js
+++ src/client/game/conundrum-round/conundrum-input.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 
-import style from '../game.scss';
 import cloakService from '../../services/cloak-service';
+import style from '../game.scss';
 
 export default class ConundrumInput extends Component {
+    componentWillMount() {
+        this.setState({
+            submitted: false
+        });
+    }
+
     submitAnswer() {
         cloakService.messageSubmitAnagram(this.state.answer);
+        this.setState({
+            submitted: true
+        });
     }
 
     handleChange(event) {
@@ -13,10 +22,11 @@ export default class ConundrumInput extends Component {
             answer: event.target.value
         });
     }
+
     render() {
         const submitButton = (
             <div>
-                <button className={`btn btn-success ${style['submit-button']}`} onClick={() => this.submitAnswer()}>Submit</button>
+                <button className={`btn btn-success ${style['submit-button']}`} onClick={() => this.submitAnswer.bind(this)()}>Submit</button>
             </div>
         );
 
@@ -25,7 +35,7 @@ export default class ConundrumInput extends Component {
                 <h3>Answer</h3>
                 <input maxLength="20" size="30" placeholder="Answer here"
                 onChange={(event) => this.handleChange(event)} />
-                {submitButton}
+                {!this.state.submitted ? submitButton : null}
             </div>
         );
     }
