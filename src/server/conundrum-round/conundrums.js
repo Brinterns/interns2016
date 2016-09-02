0fa2e609e6d397c6b2c332261b427d79899fd832
Raul Mihoc <rmihoc@scottlogic.co.uk>
2016-09-02 09:59:39 +0100

    filtered list of conundrums to remove duplicates and updated generator script

--- src/server/conundrum-round/conundrums.js
+++ src/server/conundrum-round/conundrums.js
@@ -1,10 +1,24 @@
 var fs = require('fs');
 var generateConundrum = require('./conundrum-generator');
 
-var l = []
+//generate 5000 conundrums
+var list = [];
 for(var i = 0 ; i < 5000; i++) {
     console.log(i);
-    l.push(generateConundrum());
+    list.push(generateConundrum());
 }
 
- fs.writeFile('./conundrum-list.js', 'module.exports = ' + JSON.stringify(l));
+//filter all duplicates
+var l = [];
+for(var i = 0; i < list.length; i++) {
+    var ok = true;
+    for(var j = 0; j < l.length; j++) {
+        if((list[i].first === l[j].first && list[i].second === l[j].second) || (list[i].first === l[j].second && list[i].second === l[j].first)) {
+            ok = false;
+        }
+    }
+    if(ok) {
+        l.push(list[i]);
+    }
+}
+fs.writeFile('./conundrum-list.js', 'module.exports = ' + JSON.stringify(l));
